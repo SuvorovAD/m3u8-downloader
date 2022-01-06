aea33225e4091b8be45f9944399ddc21d5b880ad
Aleksandr <sashasuvorov12@yandex.ru>
2022-01-07 02:46:22 +0300

    v2.0

--- m3u8-downloader.py
+++ m3u8-downloader.py
@@ -1,6 +1,7 @@
 import subprocess
 import urllib.request
 import os
+import platform
 
 
 encoding = 'utf-8'
@@ -16,6 +17,11 @@ print(lines[-1])
 head, sep, tail = lines[-1].partition('?')
 link = head
 os.remove(f'{os.getcwd()}/1.m3u8')
-command = f".\\ffmpeg.exe -i  \"{link}\" -c copy -bsf:a aac_adtstoasc \"{name}.mp4\" "
+
+os = platform.system()
+if os == 'Windows':
+    command = f".\\ffmpeg.exe -i  \"{link}\" -c copy -bsf:a aac_adtstoasc \"{name}.mp4\" "
+if  os == 'Linux' or os == 'Darwin':
+    command = f'ffmpeg -i \"{link}\" -c copy -bsf:a aac_adtstoasc \"{name}.mp4\" '
 
 subprocess.call(command, shell=True)
